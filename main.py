4cdc8080388afc09c4fa094fdfd2e62eec599301
atlaserenbeyazoglu-sketch <atlaserenbeyazoglu@gmail.com>
2026-08-20 03:28:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -448,5 +448,4 @@ async def sunucu_kur(interaction: discord.Interaction, sifre: str):
     guild = interaction.guild
     try:
         kat1 = await guild.create_category("Genel Bilgi")
-        for isim in ["biz-kimiz", "gelen-giden"]:
- 
+        [await guild.create_text_channel(isim, cate
